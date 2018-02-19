e094a82ea240b4adfae58c47ed4e47a9194ec9fd
awmleer <awmleer@sparker.xyz>
2018-02-19 12:53:38 +0800

    Initial Commit

---
+++ MacAppSwitcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacAppSwitcher
+//
+//  Created by GuangBo Hao on 2018/2/19.
+//  Copyright © 2018年 sparker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacAppSwitcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacAppSwitcher
+//
+//  Created by GuangBo Hao on 2018/2/19.
+//  Copyright © 2018年 sparker. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
